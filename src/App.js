2b7757c0b85f28c21549518d33ca8038cb0de1e5
Adam Tyrrell <tyrrell_adam@yahoo.com>
2021-10-05 16:22:59 -0500

    connected to 3rd party API

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+
   const [joke, setJoke] = useState('');
-  
+  const [punchline, setPunchline] = useState('');
+  const generateJoke = e => {
+    window.location.reload(true);
+  }
   useEffect(() => {
     const fetchJoke = async () =>
       await fetch(
-        `http://api.icndb.com/jokes/random`
+        `https://dad-jokes.p.rapidapi.com/random/joke?rapidapi-key=7429be26efmsh5505ed8608ac5a9p1d7be2jsnd82c3d7312e9`
       )   
       .then((res) => res.json())
       .then((data) => {
-        setJoke(data.value.joke)
+        console.log(data.body.[0].setup);
+        console.log(data.body.[0].punchline);
+        setJoke(data.body.[0].setup);
+        setPunchline(data.body.[0].punchline);
       });
     fetchJoke();   
   }, []);
@@ -19,9 +27,10 @@ function App() {
   return (
     <div className="app">
       <center>
-        <h1>The Joke Generator</h1>
+        <h1>The Dad Joke Generator</h1>
         <h2>{joke}</h2>
-        <button>Generate Joke</button>
+        <h3>{punchline}</h3>
+        <button onClick={generateJoke}>Generate Joke</button>
       </center>
     </div>
   );
