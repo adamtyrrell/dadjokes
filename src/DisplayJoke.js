b8a692ed8e4774c3f73f0495b305b3dcd1f9fa6f
Adam Tyrrell <tyrrell_adam@yahoo.com>
2021-10-14 00:27:56 -0500

    error handeling complete

--- src/DisplayJoke.js
+++ src/DisplayJoke.js
@@ -2,37 +2,39 @@ import React, { useEffect, useState } from "react";
 import { newIndex } from "./App";
 import './DisplayJoke.css';
 
-
 function DisplayJoke() {
     const [isLoading, setIsLoading] = useState(false);
     const [hasError, setHasError] = useState(false);
     const [joke, setJoke] = useState('');
     useEffect((e) => {
-    setIsLoading(true);
-    const fetchJoke = async () => 
-        await fetch(
-        `http://localhost:3000/dadjokes/${newIndex}`
-        )   
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            setJoke(data);
+        setIsLoading(true);
+        setHasError(false);
+        const fetchJoke = async () => {
+            try {
+                await fetch(
+                `http://localhost:3000/dadjokes/${newIndex}`
+                )   
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    setJoke(data);
+                })
+            } catch (error) {
+                setHasError (true);
+            }
             setIsLoading(false); 
-            console.log(isLoading);
-        });
-    fetchJoke();  
-    }, []);
+        };
+            fetchJoke();  
+        }, []);
 
     return (
         <React.Fragment>
+            {hasError && <p>Whoops! Something went wrong!</p>}
             {isLoading ? (<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <circle cx="50" cy="50" r="45"/>
             </svg>) : (<div><h2>{joke.setup}</h2><h3>{joke.punchline}</h3></div>)}
         </React.Fragment>
-
     );
-
-
 }
 
 
