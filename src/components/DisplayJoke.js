fdc174d0aa8a2f83b1f72a6822450ae369b0f142
Adam Tyrrell <tyrrell_adam@yahoo.com>
2021-10-20 00:09:28 -0500

    button moved outside of card

--- src/components/DisplayJoke.js
+++ src/components/DisplayJoke.js
@@ -33,9 +33,9 @@ function DisplayJoke() {
     return (
         <React.Fragment>
             {hasError && <p>Whoops! Something went wrong!</p>}
-            {isLoading ? (<svg className="loader" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            {isLoading ? (<svg className="loader m-10" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <circle cx="50" cy="50" r="45"/>
-            </svg>) : (<div><h2>{joke.setup}</h2><h3>{joke.punchline}</h3>
+            </svg>) : (<div><h2>{joke.setup}</h2><div className="mt-5 mb-5"><h3 className="italic">{joke.punchline}</h3></div>
             <div className="flex justify-evenly max-w-sm m-3"><ThumbUpIcon className="h-8 m-3 hover:cursor-pointer hover:animate-pulse"/><h5 className="m-3">Likes: {joke.likes}</h5><ShareIcon className="h-8 m-3 hover:cursor-pointer hover:animate-pulse"/></div></div>)}
         </React.Fragment>
     );
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -24,14 +24,13 @@ function Home() {
           <center className="mt-16">
             <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical" flipSpeedBackToFront="1" flipSpeedFrontToBack="1">
               <div>
-                <div className="card-text">{joke}</div>      
-                  <button onClick={handleFlip}className="bg-yellow-400 hover:bg-yellow-300 text-black font-bold py-2 px-4 border border-yellow-500 rounded">Generate Joke</button>
+                <div className="card-text">{joke}</div>                      
                 </div>
               <div>
-                <div className="card-text">{joke}</div>      
-                <button onClick={handleFlip} className="bg-yellow-400 hover:bg-yellow-300 text-black font-bold py-2 px-4 border border-yellow-500 rounded">Generate Joke</button>
+                <div className="card-text">{joke}</div>                    
               </div>
           </ReactCardFlip>
+          <button onClick={handleFlip} className="mt-5 bg-yellow-400 hover:bg-yellow-300 text-black font-bold py-2 px-4 border border-yellow-500 rounded">Generate Joke</button>
         </center>
 
     )
